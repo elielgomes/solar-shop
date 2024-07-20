4e6d4cbd648bd905f3114044f113e8c5b218c1c6
Eliel Gomes Hyertquist <elielgomespg@hotmail.com>
2024-07-20 08:07:54 -0300

    :sparkles: feat: Add categories constants

---
+++ src/constants/categories.ts
@@ -0,0 +1,34 @@
+export const categories = [
+  {
+    title: "Paineis",
+    slug: "paineis",
+  },
+  {
+    title: "Inversores",
+    slug: "inversores",
+  },
+  {
+    title: "Estruturas",
+    slug: "estruturas",
+  },
+  {
+    title: "Cabos",
+    slug: "cabos",
+  },
+  {
+    title: "Conectores",
+    slug: "conectores",
+  },
+  {
+    title: "Fusíveis",
+    slug: "fusiveis",
+  },
+  {
+    title: "String Box",
+    slug: "string-box",
+  },
+  {
+    title: "Transformadores",
+    slug: "transformadores",
+  },
+] as const;
