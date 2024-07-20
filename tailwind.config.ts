d959481e1eb065215e6e8fcca0e65efb9f222cab
Eliel Gomes Hyertquist <elielgomespg@hotmail.com>
2024-07-20 08:01:57 -0300

    :sparkles: feat: Update Tailwind CSS configuration
    This commit updates the Tailwind CSS configuration file (`tailwind.config.ts`).
    
    Changes include:
    - Adjusting the padding values for the `container` theme to provide more consistent spacing across different screen sizes.
    - Updating the screen breakpoints for the `container` theme to align with common device sizes.
    
    These changes aim to improve the overall layout and responsiveness of the application.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,29 @@
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
 
 const config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-	],
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
   prefix: "",
   theme: {
     container: {
       center: true,
-      padding: "2rem",
+      padding: {
+        DEFAULT: "1rem",
+        sm: "1.5rem",
+        md: "2rem",
+        lg: "3rem",
+        xl: "4rem",
+      },
       screens: {
-        "2xl": "1400px",
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
       },
     },
     extend: {
@@ -75,6 +84,6 @@ const config = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config
+} satisfies Config;
 
-export default config
+export default config;
