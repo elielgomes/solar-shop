5813173ba2d8593fcb30e508534fc5f72389ef26
Eliel Gomes Hyertquist <elielgomespg@hotmail.com>
2024-07-21 11:04:32 -0300

    :sparkles: feat: Add response text to current search parameter

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,15 +1,11 @@
 import { NextPage } from "next";
+import { Search } from "lucide-react";
 
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-} from "@/components/ui/select";
 import { product } from "@/services/product";
 import { ProductCard } from "@/components/product-card";
 import { FiltersSheet } from "@/app/products/_components/filters-sheet";
 import { CategoriesSidebar } from "@/app/products/_components/categories-sidebar";
+import { SelectSortProducts } from "@/app/products/_components/select-sort-products";
 import { ProductListPagination } from "@/app/products/_components/product-list-pagination";
 
 interface ProductsPageProps {
@@ -28,29 +24,30 @@ const ProductsPage: NextPage<ProductsPageProps> = async ({ searchParams }) => {
       return acc;
     }, "");
   }
-
   const productList = await product.getAll(queryParams);
 
   return (
     <main className="flex min-h-screen flex-col items-center mt-16 md:mt-28">
-      <div className="container grid grid-cols-1 lg:grid-cols-4 xl:grid-cols-5 gap-x-8 gap-y-10 py-12">
-        <div className="col-span-full flex justify-between">
-          <h1 className="text-2xl font-bold">Produtos</h1>
-          <div className="flex items-center gap-4">
-            <Select>
-              <SelectTrigger className="bg-card  h-full shadow-md shadow-gray-600/5 border border-border rounded outline-none focus:ring-offset-0 focus:ring-0 transition-colors gap-2">
-                <span>Ordenar por</span>
-              </SelectTrigger>
-              <SelectContent>
-                <SelectItem value="asc-price">Maior Preço</SelectItem>
-                <SelectItem value="desc-price">Menor Preço</SelectItem>
-                <SelectItem value="asc">De A-Z</SelectItem>
-                <SelectItem value="desc">De Z-A</SelectItem>
-              </SelectContent>
-            </Select>
-
-            <FiltersSheet />
+      <div className="container grid grid-cols-1 lg:grid-cols-4 xl:grid-cols-5 gap-x-8 gap-y-8 py-12">
+        <div className="col-span-full">
+          <div className="flex justify-between">
+            <h1 className="text-2xl font-bold">Produtos</h1>
+            <div className="flex items-center gap-4">
+              <SelectSortProducts />
+              <FiltersSheet />
+            </div>
           </div>
+          {searchParams?.search && (
+            <div className="flex gap-4 col-span-full mt-4">
+              <p className="font-bold flex items-center gap-2">
+                <Search className="size-4" />
+                Resultados para:
+              </p>
+              <p className="font-semibold text-muted-foreground">
+                {searchParams.search}
+              </p>
+            </div>
+          )}
         </div>
         <hr className="col-span-full" />
         <CategoriesSidebar />
