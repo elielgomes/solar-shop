e17407752e9a8cbcd402e2bd3e917eb9e70aeac0
Eliel Gomes Hyertquist <elielgomespg@hotmail.com>
2024-07-21 13:13:34 -0300

    :sparkles: feat: Update FiltersSheet component to include new UI enhancements and functionality

--- src/app/products/_components/filters-sheet/index.tsx
+++ src/app/products/_components/filters-sheet/index.tsx
@@ -1,48 +1,76 @@
-import React from "react";
-import { Filter } from "lucide-react";
+"use client";
+
+import React, { useEffect, useState } from "react";
+import { Filter, FilterX } from "lucide-react";
+import { useSearchParams } from "next/navigation";
 
 import {
   Sheet,
-  SheetClose,
   SheetContent,
   SheetDescription,
-  SheetFooter,
   SheetHeader,
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
 import { Button } from "@/components/ui/button";
-import { CategoriesFilter } from "@/app/products/_components/categories-filter";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import { ProductSearchInput } from "@/components/product-search-input";
+import { CategoriesFilter } from "@/app/products/_components/categories-filter";
+import { SelectSortProducts } from "@/app/products/_components/select-sort-products";
+import Link from "next/link";
+import { routesMap } from "@/constants/routes-map";
 
 export const FiltersSheet: React.FC = () => {
+  const searchParams = useSearchParams();
+  const [isOpen, setIsOpen] = useState(false);
+  const hasFilters =
+    searchParams.get("search") ||
+    searchParams.get("category") ||
+    searchParams.get("sort");
+
+  useEffect(() => {
+    setIsOpen(false);
+  }, [searchParams]);
+
   return (
-    <Sheet>
+    <Sheet onOpenChange={setIsOpen} open={isOpen}>
       <SheetTrigger asChild>
         <Button
           className="lg:hidden bg-card text-foreground shadow-md shadow-gray-600/5 border border-border rounded"
           variant="ghost"
         >
-          <Filter className="size-5" />
+          <Filter className="size-4" />
         </Button>
       </SheetTrigger>
-      <SheetContent>
-        <SheetHeader>
-          <SheetTitle>Filtrar produtos</SheetTitle>
-          <SheetDescription>Otimize sua busca por produtos.</SheetDescription>
-        </SheetHeader>
-        <div className="space-y-8 mt-8">
-          <ProductSearchInput />
-          <div>
-            <p className="font-semibold text-sm mb-4">Categorias</p>
-            <CategoriesFilter />
+      <SheetContent className="w-full p-0">
+        <ScrollArea className="h-screen p-6">
+          <SheetHeader>
+            <SheetTitle>Filtrar produtos</SheetTitle>
+            <SheetDescription>Otimize sua busca por produtos.</SheetDescription>
+          </SheetHeader>
+          <div className="space-y-8 mt-8">
+            <ProductSearchInput />
+            {hasFilters && (
+              <div>
+                <Link
+                  href={routesMap.products}
+                  className="flex items-center gap-2 font-semibold text-sm mb-4 text-destructive bg-destructive/5 p-2 rounded"
+                >
+                  <FilterX className="size-4" />
+                  Limpar fitros
+                </Link>
+              </div>
+            )}
+            <div>
+              <p className="font-semibold text-sm mb-4">Ordenar</p>
+              <SelectSortProducts />
+            </div>
+            <div>
+              <p className="font-semibold text-sm mb-4">Categorias</p>
+              <CategoriesFilter />
+            </div>
           </div>
-        </div>
-        <SheetFooter className="sm:justify-start mt-6">
-          <SheetClose asChild>
-            <Button type="button">Ver resultados</Button>
-          </SheetClose>
-        </SheetFooter>
+        </ScrollArea>
       </SheetContent>
     </Sheet>
   );
