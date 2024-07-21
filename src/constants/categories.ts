90b7149d5d89ba34b1ce2d2ca7423cf17eac0c1f
Eliel Gomes Hyertquist <elielgomespg@hotmail.com>
2024-07-21 09:59:49 -0300

    :sparkles: feat: Add href property to category objects in categories.ts
    This commit adds the `href` property to each category object in the `categories.ts` file. The `href` property is a function that generates the URL for each category based on its slug. This change allows for dynamic and customizable category URLs in the application.

--- src/constants/categories.ts
+++ src/constants/categories.ts
@@ -1,34 +1,46 @@
+import { routesMap } from "@/constants/routes-map";
+
+const hrefCategory = (slug: string) => `${routesMap.products}?category=${slug}`;
+
 export const categories = [
   {
     title: "Paineis",
     slug: "paineis",
+    href: hrefCategory("paineis"),
   },
   {
     title: "Inversores",
     slug: "inversores",
+    href: hrefCategory("inversores"),
   },
   {
     title: "Estruturas",
     slug: "estruturas",
+    href: hrefCategory("estruturas"),
   },
   {
     title: "Cabos",
     slug: "cabos",
+    href: hrefCategory("cabos"),
   },
   {
     title: "Conectores",
     slug: "conectores",
+    href: hrefCategory("conectores"),
   },
   {
     title: "Fusíveis",
     slug: "fusiveis",
+    href: hrefCategory("fusiveis"),
   },
   {
     title: "String Box",
     slug: "string-box",
+    href: hrefCategory("string-box"),
   },
   {
     title: "Transformadores",
     slug: "transformadores",
+    href: hrefCategory("transformadores"),
   },
 ] as const;
