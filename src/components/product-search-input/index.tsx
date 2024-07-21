3d6ddb4fd842f40336bfa791aed50839436c12c3
Eliel Gomes Hyertquist <elielgomespg@hotmail.com>
2024-07-21 09:59:10 -0300

    :sparkles: feat: Update ProductSearchInput component for improved search functionality

--- src/components/product-search-input/index.tsx
+++ src/components/product-search-input/index.tsx
@@ -1,9 +1,13 @@
-import React from "react";
-import { Search } from "lucide-react";
+"use client";
 
+import { Search } from "lucide-react";
+import React, { useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+
+import { cn } from "@/lib/tw-merge";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { cn } from "@/lib/tw-merge";
+import { routesMap } from "@/constants/routes-map";
 
 interface ProductSearchInputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {}
@@ -12,21 +16,44 @@ export const ProductSearchInput: React.FC<ProductSearchInputProps> = ({
   className,
   ...props
 }) => {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+
+  const [searchInput, setSearchInput] = useState<string>("");
+
+  const updateURLParams = (text: string) => {
+    const params = new URLSearchParams(searchParams);
+    params.delete("page");
+    params.set("search", text);
+    params.get("search")?.trim() === "" && params.delete("search");
+    router.replace(`${routesMap.products}?${params.toString()}`);
+  };
+
+  const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchInput(event.target.value);
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    updateURLParams(searchInput);
+  };
+
   return (
-    <div className="relative">
+    <form className="relative" onSubmit={handleSubmit}>
       <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
         <Search />
       </div>
       <Input
         {...props}
         type="search"
-        id="default-search"
         className={cn(
           "block w-full ps-10 text-sm focus-visible:ring-0 focus-visible:ring-offset-0",
           className
         )}
         placeholder="Busque por produtos..."
         required
+        value={searchInput}
+        onChange={handleInput}
       />
       <Button
         type="submit"
@@ -34,6 +61,6 @@ export const ProductSearchInput: React.FC<ProductSearchInputProps> = ({
       >
         Buscar
       </Button>
-    </div>
+    </form>
   );
 };
