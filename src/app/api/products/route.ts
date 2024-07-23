eb39bdcd41da4fadfb2276d1919e54726e48a29c
Eliel Gomes Hyertquist <elielgomespg@hotmail.com>
2024-07-23 06:30:31 -0300

    :bug: fix: Fix sorting bug in sortProducts function

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -34,9 +34,9 @@ export const enrichProductWithCategory = (
 const sortProducts = (products: Product[], sort: string): Product[] => {
   switch (sort) {
     case "asc-price":
-      return products.sort((a, b) => a.price - b.price);
-    case "desc-price":
       return products.sort((a, b) => b.price - a.price);
+    case "desc-price":
+      return products.sort((a, b) => a.price - b.price);
     case "asc":
       return products.sort((a, b) => a.name.localeCompare(b.name));
     case "desc":
