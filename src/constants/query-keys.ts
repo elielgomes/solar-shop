3f758f931a829d3059c12cb80ce1797b269a83e5
Eliel Gomes Hyertquist <elielgomespg@hotmail.com>
2024-07-23 09:18:23 -0300

    :sparkles: feat: Add productFilters constant for managing product query filters

--- src/constants/query-keys.ts
+++ src/constants/query-keys.ts
@@ -2,4 +2,5 @@ export const queryKeys = {
   product: ["product"],
   categories: ["categories"],
   productById: (id: string) => ["product", id],
+  productFilters: (queryParams: string) => ["products", queryParams],
 } as const;
